cb213be29d2218f5c1f7a006cda3c99d07c9d1fa
Austin <austephner@gmail.com>
2022-06-28 14:00:15 -0400

    upload

---
+++ Assets/Editor/SmoothBlendShapeBehaviourEditor.cs
@@ -0,0 +1,57 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(SmoothBlendShapeBehaviour))]
+    public class SmoothBlendShapeBehaviourEditor : UnityEditor.Editor
+    {
+        private int _index;
+
+        private float _value;
+
+        private string _name;
+        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUILayout.Space(15);
+            GUILayout.Label("Tools", EditorStyles.boldLabel);
+
+            GUILayout.Label("Set Blend Shape by Index", EditorStyles.wordWrappedMiniLabel);
+            using (new GUILayout.HorizontalScope())
+            {
+                GUILayout.Label("Index");
+                GUILayout.Space(5);
+                _index = EditorGUILayout.IntField(_index);
+                GUILayout.Space(15);
+                GUILayout.Label("Value");
+                GUILayout.Space(5);
+                _value = EditorGUILayout.FloatField(_value);
+                if (GUILayout.Button("Set"))
+                {
+                    ((SmoothBlendShapeBehaviour) target).SetBlendShapeWeight(_index, _value);
+                }
+            }
+
+            GUILayout.Space(5);
+            
+            GUILayout.Label("Set Blend Shape by Name", EditorStyles.wordWrappedMiniLabel);
+            using (new GUILayout.HorizontalScope())
+            {
+                GUILayout.Label("Name");
+                GUILayout.Space(5);
+                _name = EditorGUILayout.TextField(_name);
+                GUILayout.Space(15);
+                GUILayout.Label("Value");
+                GUILayout.Space(5);
+                _value = EditorGUILayout.FloatField(_value);
+                if (GUILayout.Button("Set"))
+                {
+                    ((SmoothBlendShapeBehaviour) target).SetBlendShapeWeight(_name, _value);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SmoothBlendShapeBehaviour.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothBlendShapeBehaviour : MonoBehaviour
+{
+    [SerializeField] private SkinnedMeshRenderer _smr;
+
+    [SerializeField] private float _blendShapeTransitionSpeed = 3.0f;
+    
+    private List<float> _targetWeights = new List<float>();
+
+    private void Start() 
+    {
+        for (int i = 0; i < _smr.sharedMesh.blendShapeCount; i++) 
+        {
+            _targetWeights.Add(_smr.GetBlendShapeWeight(i));
+        }
+    }
+
+    private void Update() 
+    {
+        for (int i = 0; i < _targetWeights.Count; i++) 
+        {
+            // you could use Lerp here instead of MoveTowards if you want
+    
+            _smr.SetBlendShapeWeight(
+                i, 
+                Mathf.MoveTowards(
+                    _smr.GetBlendShapeWeight(i),
+                    _targetWeights[i],
+                    Time.deltaTime * _blendShapeTransitionSpeed));
+        }
+    }
+
+    public void SetBlendShapeWeight(string blendShapeName, float value) 
+    {
+        SetBlendShapeWeight(
+            _smr.sharedMesh.GetBlendShapeIndex(blendShapeName), 
+            value);
+    }
+
+    public void SetBlendShapeWeight(int blendShapeIndex, float value) 
+    {
+        _targetWeights[blendShapeIndex] = value;
+    }
+}
